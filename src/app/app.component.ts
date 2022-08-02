f27278b35d40ad61fadba508e369a583286d43d7
Abir Mahmud <amabirmahmud@gmail.com>
2022-08-03 02:17:05 +0600

    Sync store in all tabs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import { fromEvent } from "rxjs";
 import { Store } from "@ngxs/store";
 import * as lodash from "lodash"
 
+import CryptoJS from 'crypto-js';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -21,9 +23,7 @@ export class AppComponent {
     let source$ = fromEvent<StorageEvent>(window, 'storage');
 
     source$.subscribe(data => {
-      console.log(this.store.snapshot());
-      console.log(JSON.parse(data.newValue))
-      console.log(this.store.snapshot() == JSON.parse(data.newValue))
+      this.store.reset(JSON.parse(CryptoJS.AES.decrypt(localStorage['@@STATE'],"SECRET").toString(CryptoJS.enc.Utf8)));
     });
 
   }
--- src/app/read/read.component.ts
+++ src/app/read/read.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { Select, Store } from "@ngxs/store";
 import { TutorialState } from "../state/tutorial.state";
-import { Observable } from "rxjs";
+import { Observable, of } from "rxjs";
 import { Tutorial } from "../models/tutorial.model";
 import { TutorialAction } from "../actions/tutorial.action";
+
+import CryptoJS from 'crypto-js';
+
 import RemoveTutorial = TutorialAction.RemoveTutorial;
 
 @Component({
@@ -15,7 +18,8 @@ export class ReadComponent implements OnInit {
 
   @Select(TutorialState.getTutorials) tutorials$: Observable<Tutorial[]>;
 
-  constructor(private store: Store) { }
+  constructor(private store: Store) {
+  }
 
   ngOnInit(): void {
   }
@@ -25,4 +29,7 @@ export class ReadComponent implements OnInit {
   }
 
 
+  refreshTutorials() {
+      this.store.reset(JSON.parse(CryptoJS.AES.decrypt(localStorage['@@STATE'],"SECRET").toString(CryptoJS.enc.Utf8)));
+  }
 }
